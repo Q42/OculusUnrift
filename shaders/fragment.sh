f2ac6f12280dc3e05cf5d1c8a6b28e4c8f1519f6
marcelduin <marcel@q42.nl>
2013-11-08 16:20:28 +0100

    better sizing/centering + marginfixes overlay

--- shaders/fragment.sh
+++ shaders/fragment.sh
@@ -10,15 +10,14 @@ uniform sampler2D tex2;
 varying highp vec2 vTextureCoord;
 varying vec2 p;
 
-vec2 res = vec2(640.,480.);
+vec2 res = vec2(1280.,480.);
 float pi = 3.14159265358979323846264;
 
 // voor w00tcamp oculus unrift
 const vec2 Scale = vec2(0.1469278, 0.2350845);
 const vec2 ScaleIn = vec2(4, 2.5);
 const vec4 HmdWarpParam = vec4(1, 0.25, 0.25, 0);
-const vec2 LeftLensCenter = vec2(0.2863248, 0.5);
-const vec2 RightLensCenter = vec2(0.7136753, 0.5);
+
 const vec2 LeftScreenCenter = vec2(0.25, 0.5);
 const vec2 RightScreenCenter = vec2(0.75, 0.5);
 
@@ -34,12 +33,11 @@ vec2 HmdWarp(vec2 in01, vec2 LensCenter) {
 
 void main() {
 	bool left = (gl_FragCoord.x < res.x/2.0);
-	vec2 LensCenter = left ? LeftLensCenter : RightLensCenter;
 	vec2 ScreenCenter = left ? LeftScreenCenter : RightScreenCenter;
 	vec2 otc = (gl_FragCoord.xy) / vec2(res.x, res.y);
 
   //warp that shit
-	vec2 tc = HmdWarp(otc, LensCenter);
+	vec2 tc = HmdWarp(otc, ScreenCenter);
 
   //todo - better area clamping
   if (!(any(bvec2(clamp(tc,ScreenCenter-vec2(0.25,0.5), ScreenCenter+vec2(0.25,0.5)) - tc))))
