8655470a0688de3a0eb3562222fd3553b345995f
huijkman <johan@q42.nl>
2013-11-08 16:02:48 +0100

    included sounds

--- js/overlay.js
+++ js/overlay.js
@@ -1,22 +1,50 @@
 function Overlay(num) {
-	this.canvas = document.getElementById('ol'+num);
+	/*this.canvas = document.getElementById('ol'+num);
 	var ctx = this.canvas.getContext('2d');
 	ctx.fillStyle = '#ff0000';
 	ctx.fillRect(10,10,200,200);
 	ctx.fillStyle = '#00ff00';
 	ctx.fillRect(0,0,200,200);
 	ctx.fillStyle = '#0000ff';
-	ctx.fillRect(-10,-10,200,200);
+	ctx.fillRect(-10,-10,200,200);*/
 };
 
 
 /* ===== CANVAS 2D */
 
 $(function(){
-	var canvas = document.getElementById("hud-overlay"),
+	canvas = document.getElementById("hud-overlay");
 	ctx = canvas.getContext("2d");
 
-	var fps = 15;
+	scene1();
+	/*(function() {
+	    var lastTime = 0;
+	    var vendors = ['ms', 'moz', 'webkit', 'o'];
+	    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+	        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+	        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+	                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+	    }
+	 
+	    if (!window.requestAnimationFrame)
+	        window.requestAnimationFrame = function(callback, element) {
+	            var currTime = new Date().getTime();
+	            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+	            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+	              timeToCall);
+	            lastTime = currTime + timeToCall;
+	            return id;
+	        };
+	 
+	    if (!window.cancelAnimationFrame)
+	        window.cancelAnimationFrame = function(id) {
+	            clearTimeout(id);
+	        };
+	}());*/
+})
+
+var scene1 = function(){
+	var fps = 30;
 	var frame = 0;
 	var time = 1000/fps;
 
@@ -43,8 +71,8 @@ $(function(){
 			ctx.fillStyle = '#ffffff';
 		    ctx.fillText(frame, 600, 460);
 
-
 		    blurp += texts[text_index].charAt(char_index);
+		    createjs.Sound.play('sounds/blip.mp3');
 			char_index += 1;
 
 			ctx.font = '14pt "visitor_tt1_brkregular" normal';
@@ -52,40 +80,41 @@ $(function(){
 		    ctx.fillText(blurp, 50, 100);
 		    if(char_index == texts[text_index].length) {
 	    		char_index = 0;
-				if(text_index<texts.length-1) text_index++;
-				else text_index=0;
-				blurp = '';
-				setTimeout(draw,2500);
+				if(text_index<texts.length-1){
+					text_index++;
+					blurp = '';
+					setTimeout(draw,2500);
+				}
+				else {
+					setTimeout(scene2,2500);
+				}
+				
 			}else{
 				draw();
 			}
 	    }, time);
 	}
 	setTimeout(draw,100);
-	 
-	(function() {
-	    var lastTime = 0;
-	    var vendors = ['ms', 'moz', 'webkit', 'o'];
-	    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-	        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-	        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-	                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-	    }
-	 
-	    if (!window.requestAnimationFrame)
-	        window.requestAnimationFrame = function(callback, element) {
-	            var currTime = new Date().getTime();
-	            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-	            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-	              timeToCall);
-	            lastTime = currTime + timeToCall;
-	            return id;
-	        };
-	 
-	    if (!window.cancelAnimationFrame)
-	        window.cancelAnimationFrame = function(id) {
-	            clearTimeout(id);
-	        };
-	}());
-})
+}
+
+var scene2 = function(){
+	// Clear the canvas
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	ctx.font = '24pt "visitor_tt1_brkregular" normal';
+	ctx.fillStyle = '#ffffff';
+    ctx.fillText('scene 2', 280, 220);
+};
+
+addEventListener('DOMContentLoaded', function() {
+	var manifest = [
+		{ id: 'boot', src: 'sounds/robotpoweron.mp3' },
+		{ id: 'blip', src: 'sounds/blip.mp3' }
+	];
+	createjs.Sound.addEventListener('fileload', handleLoad);
+	createjs.Sound.registerManifest(manifest);
+	function handleLoad(event) {
+		console.log('loaded ' + event.src);
+	}
+});
 
