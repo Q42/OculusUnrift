9b29d2b89ab29afdb7b5a560caa3c61c586e3736
marcelduin <marcel@q42.nl>
2013-11-08 11:16:42 +0100

    spaces to tabs

--- shaderdev/js/webcam.js
+++ shaderdev/js/webcam.js
@@ -1,61 +1,55 @@
 addEventListener('DOMContentLoaded',function() {
-  var streaming = false,
-      width = 320,
-      height = 0;
+	var streaming = false,
+			width = 320,
+			height = 0;
 
-  navigator.getMedia = ( navigator.getUserMedia ||
-                         navigator.webkitGetUserMedia ||
-                         navigator.mozGetUserMedia ||
-                         navigator.msGetUserMedia);
-                         
-  MediaStreamTrack.getSources(function(l){
-    var vids = [];
-    for(var i=0;i<l.length;i++)
-      if(l[i].kind=='video') {
-        vids.push(l[i].id);
-      }
-    vidStream(1,vids);
-    vidStream(2,vids);
-  });
+	navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+	MediaStreamTrack.getSources(function(l){
+		var vids = [];
+		for(var i=0;i<l.length;i++)
+			if(l[i].kind=='video') {
+				vids.push(l[i].id);
+			}
+		vidStream(1,vids);
+		vidStream(2,vids);
+	});
 
 });
 
 function vidStream(num,ids) {
-  console.log('new stream...',num,ids);
+	var video = document.querySelector('#vid'+num);
 
-  var video = document.querySelector('#vid'+num);
+	var _sel = document.createElement('select');
+	document.body.appendChild(_sel);
 
-  var _sel = document.createElement('select');
-  document.body.appendChild(_sel);
+	var _none = document.createElement('option');
+	_none.text = 'select for '+num;
+	_sel.appendChild(_none);
 
-  var _none = document.createElement('option');
-  _none.text = 'select for '+num;
-  _sel.appendChild(_none);
+	for(var i=0;i<ids.length;i++) {
+		var _opt = document.createElement('option');
+		_opt.value = ids[i];
+		_opt.text = i;
+		_sel.appendChild(_opt);
+	}
 
-  for(var i=0;i<ids.length;i++) {
-    var _opt = document.createElement('option');
-    _opt.value = ids[i];
-    _opt.text = i;
-    _sel.appendChild(_opt);
-  }
+	_sel.addEventListener('change',function(){
+		if(_sel.value) startStream(video,_sel.value);
+	});
 
-  
-  _sel.addEventListener('change',function(){
-    if(_sel.value) startStream(video,_sel.value);
-  });
-  
-  function startStream(video,id){
-    navigator.getMedia({
-      video: {optional: [{sourceId: id}]},
-      audio: false
-    },function(stream) {
-      if(navigator.mozGetUserMedia)
-        video.mozSrcObject = stream;
-      else
-        video.src = (window.URL || window.webkitURL).createObjectURL(stream);
-      video.play();
-    },function(err) {
-      console.log("An error occured! " + err);
-    });
-  };
+	function startStream(video,id){
+		navigator.getMedia({
+			video: {optional: [{sourceId: id}]},
+			audio: false
+		},function(stream) {
+			if(navigator.mozGetUserMedia)
+				video.mozSrcObject = stream;
+			else
+				video.src = (window.URL || window.webkitURL).createObjectURL(stream);
+			video.play();
+		},function(err) {
+			console.log("An error occured! " + err);
+		});
+	};
 };
