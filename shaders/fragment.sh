47195df4b450fbccf164a5c20d25b33d0801a084
marcelduin <marcel@q42.nl>
2013-11-08 18:12:14 +0100

    inited scene shader packing

--- shaders/fragment.sh
+++ shaders/fragment.sh
@@ -6,6 +6,7 @@ precision highp float;
 uniform sampler2D overlay;
 uniform sampler2D tex1;
 uniform sampler2D tex2;
+uniform int scene;
 
 varying highp vec2 vTextureCoord;
 varying vec2 p;
@@ -30,11 +31,17 @@ vec2 HmdWarp(vec2 in01, vec2 LensCenter) {
 			HmdWarpParam.w * rSq * rSq * rSq);
 	 return LensCenter + Scale * rvector;
 }
+//shader scene 1
+vec4 bla(vec4 color){
+	return vec4(1.,0.,0.,1.);
+}
 
 void main() {
 	bool left = (gl_FragCoord.x < res.x/2.0);
 	vec2 ScreenCenter = left ? LeftScreenCenter : RightScreenCenter;
-	vec2 otc = (gl_FragCoord.xy) / vec2(res.x, res.y);
+	vec2 otc = gl_FragCoord.xy / res;
+
+	vec4 col;
 
 	//warp that shit
 	vec2 tc = HmdWarp(otc, ScreenCenter);
@@ -43,15 +50,23 @@ void main() {
 	if (!(any(bvec2(clamp(tc,ScreenCenter-vec2(0.25,0.5), ScreenCenter+vec2(0.25,0.5)) - tc))))
 	{
 		vec2 dp;
+
 		if(left) {
 			dp = vec2(tc.x+0.25,tc.y);
-			gl_FragColor = texture2D(tex1,dp);
+			col = texture2D(tex1,dp);
 		}
 		else {
 			dp = vec2(tc.x-0.25,tc.y);
-			gl_FragColor = texture2D(tex2,dp);
+			col = texture2D(tex2,dp);
 		}
-		gl_FragColor+=texture2D(overlay,dp);
+
+		col+=texture2D(overlay,dp);
+
+		if(scene==1) {
+			col = bla(col);
+		}
+
+		gl_FragColor = col;
 	}
 	// black out clamp
 	else gl_FragColor = vec4(0.,0.,0.,0.);
