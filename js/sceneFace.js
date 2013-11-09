26cd223e74a65fb55e75b82ff417aee3ec034acb
Christiaan Hees <mail@chees.info>
2013-11-09 05:13:11 +0100

    optimized face tracking

--- js/sceneFace.js
+++ js/sceneFace.js
@@ -5,23 +5,22 @@ var sceneFace = function(callback) {
 	htracker.init(streams[0].video, canvas);
 	htracker.start();
 
-	var headZ = 0;
-
 	document.addEventListener('headtrackrStatus', function (event) {
 		//console.log(event);
 	});
 	document.addEventListener('facetrackingEvent', function(event) {
 		//console.log(event.x, event.y);
 		hud.beginPath();
-		var size = 200 - headZ;
-		hud.rect(event.x - size / 2, event.y - size / 2, size, size);
-		hud.lineWidth = 5;
+		hud.moveTo(event.x - 20, event.y);
+		hud.lineTo(event.x + 20, event.y);
+		hud.moveTo(event.x, event.y - 25);
+		hud.lineTo(event.x, event.y + 25);
+		hud.lineWidth = 2;
 		hud.strokeStyle = 'red';
 		hud.stroke();
 	}, false);
 	document.addEventListener('headtrackingEvent', function(event) {
-		headZ = event.z;
-		//console.log(headZ);
+		//console.log(event.z);
 	}, false);
 
 	setTimeout(function() {
